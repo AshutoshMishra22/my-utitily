d6705df0aa76053975c377dcf7692862db739e4f
AshutoshMishra22 <ashutoshmishraofficial22@gmail.com>
2024-08-25 10:18:02 +0530

    navigation added

--- src/component/Navigation/Navigation.tsx
+++ src/component/Navigation/Navigation.tsx
@@ -1,14 +1,17 @@
 import { FC } from "react";
-import { Navigate, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import "./Navigation.scss";
 
 type Props = {};
 
 const Navigation: FC = ({}: Props) => {
   return (
-    <nav>
-      <NavLink to="/">Go to HOMEPAGE</NavLink>
-      <> </>
-      <NavLink to="/user">Go to USER</NavLink>
+    <nav className="navigation-container">
+      <img src="" alt="logo" />
+      <section className="navigation-item-list">
+        <NavLink to="/">Go to HOMEPAGE</NavLink>
+        <NavLink to="/user">Go to USER</NavLink>
+      </section>
     </nav>
   );
 };
