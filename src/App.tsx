e885f124b9f5d522edd147dacc5d884d9cac1c9f
AshutoshMishra22 <ashutoshmishraofficial22@gmail.com>
2024-09-18 13:09:35 +0530

    iphone check added to meta

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,25 @@ import loader from "./assets/loader.png";
 import "./App.scss";
 import { updateMessage } from "./feature/slices/globalSlice";
 
+const addMaximumScaleToMetaViewport = () => {
+  const el = document.querySelector("meta[name=viewport]");
+
+  if (el !== null) {
+    let content = el.getAttribute("content") as string;
+    let re = /maximum\-scale=[0-9\.]+/g;
+
+    if (re.test(content)) {
+      content = content.replace(re, "maximum-scale=1.0");
+    } else {
+      content = [content, "maximum-scale=1.0"].join(", ");
+    }
+
+    el.setAttribute("content", content);
+  }
+};
+
+const disableIosTextFieldZoom = addMaximumScaleToMetaViewport;
+
 function App() {
   const { isLoading, message } = useAppSelector((state) => state.global);
   const dispatch = useAppDispatch();
@@ -15,9 +34,16 @@ function App() {
       dispatch(updateMessage());
     }, 3000);
   }
+  //Check if it is an iPad, iPhone or iPod
+  const checkIsIOS = () => /iPad|iPhone|iPod/.test(navigator.userAgent);
+
+  if (checkIsIOS()) {
+    disableIosTextFieldZoom();
+  }
   return (
     <div className="app-container">
       <BrowserRouter>
+        <h1>{checkIsIOS()}</h1>
         {isLoading && (
           <img src={loader} alt="loading ... " className="app-loader" />
         )}
